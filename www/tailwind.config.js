765a81829d80a4cb72315e45ad045eb47a277620
Jimmy Bisenius <me@jimmybweb.com>
2020-09-05 01:26:02 -0400

    Updated tailwind config

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
   theme: {},
   variants: {},
   plugins: [],
+  experimental: {
+    darkModeVariant: true,
+    uniformColorPalette: true,
+  },
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
