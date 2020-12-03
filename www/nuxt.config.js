7ebcacc9693c2547cb2e6e1370c72db6faa1ed81
Jim Bisenius <jim@neutroncreative.com>
2020-12-03 12:08:38 -0500

    Update marketing assets

--- www/nuxt.config.js
+++ www/nuxt.config.js
@@ -4,11 +4,21 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'www',
+    title: 'Neutron | Better websites in one click',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Neutron is the ultimate automated webmaster tool combined with an intuitive workflow, making your websites faster, more secure, and more affordable in one-click.' },
+		{ hid: 'og:type', name: 'og:type', content: 'website' },
+		{ hid: 'og:url', name: 'og:url', content: 'https://neutron.so' },
+		{ hid: 'og:title', name: 'og:title', content: 'Neutron | Better websites in one click' },
+		{ hid: 'og:description', name: 'og:description', content: 'Neutron is the ultimate automated webmaster tool combined with an intuitive workflow, making your websites faster, more secure, and more affordable in one-click.' },
+		{ hid: 'og:image', name: 'og:image', content: 'https://neutron.so/open-graph-image.png' },
+		{ hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+		{ hid: 'twitter:url', name: 'twitter:url', content: 'https://neutron.so' },
+		{ hid: 'twitter:title', name: 'twitter:title', content: 'Neutron | Better websites in one click' },
+		{ hid: 'twitter:description', name: 'twitter:description', content: 'Neutron is the ultimate automated webmaster tool combined with an intuitive workflow, making your websites faster, more secure, and more affordable in one-click.' },
+		{ hid: 'twitter:image', name: 'twitter:image', content: 'https://neutron.so/open-graph-image.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
