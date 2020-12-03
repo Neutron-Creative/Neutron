93479e62e928f78a91230de07f3242237f59b8e0
Jim Bisenius <jim@neutroncreative.com>
2020-12-03 11:32:25 -0500

    I made a few small changes...

--- www/layouts/default.vue
+++ www/layouts/default.vue
@@ -1,35 +1,37 @@
 <template>
   <div class="flex flex-col w-screen min-h-screen">
-    <div style="background:#111;" class="flex flex-row border border-black border-l-0 border-r-0 items-center justify-center p-2">
-      <span class="text-sm font-semibold text-gray-200">We've released Neutron to the public!</span>
+    <a href="https://neutroncreative.com" style="background:#111;" class="hidden lg:flex flex-row border border-black border-l-0 border-r-0 items-center justify-center p-2">
+      <span class="text-sm font-semibold text-gray-200">Neutron is a part of our mission to make the world open-source.</span>
       <span class="ml-4 mr-4 text-xs text-gray-400">|</span>
       <button class="text-sm font-semibold text-gray-200" type="button">Read more 👉</button>
-    </div>
+    </a>
     <!-- Navigation Bar -->
     <nav style="background:rgba(0,0,0,.85);" class="sticky top-0 flex w-full items-center justify-center p-4 shadow-sm nc-nav border border-t-0 border-l-0 border-r-0 border-b-0 z-50 border-gray-100">
       <div class="flex flex-row w-11/12 max-w-5xl justify-between">
         <n-link class="flex flex-row items-center justify-center mr-auto" to="/">
           <img style="width: 120px;" src="/Sidebar-brandmark.svg"/>
         </n-link>
-        <ul class="flex flex-row items-center justify-center text-gray-300 text-sm">
+        <!--
+		<ul class="flex flex-row items-center justify-center text-gray-300 text-sm">
           <n-link to="/">
             <li class="p-2 pl-4 pr-4 hover:text-gray-100 cursor-pointer rounded">Overview</li>
           </n-link>
           <n-link to="/features">
             <li class="p-2 pl-4 pr-4 hover:text-gray-100 cursor-pointer rounded">Features</li>
-          </n-link>
-          <n-link to="/community">
+		  </n-link>
+          <a href="https://neutroncreative.com/community">
             <li class="p-2 pl-4 pr-4 hover:text-gray-100 cursor-pointer rounded">Community</li>
-          </n-link>
-          <n-link to="/pricing"><li class="p-2 pl-4 pr-4 hover:text-gray-100 cursor-pointer rounded">Pricing</li></n-link>
-          <iframe style="transform: scale(.8);" src="https://ghbtns.com/github-btn.html?user=neutron-creative&repo=neutron&type=star&count=true&size=large" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
-        </ul>
-        <ul class="flex flex-row text-gray-300">
-          <a href="https://client.neutron.so">
+		  </a>
+			  <n-link to="/pricing"><li class="p-2 pl-4 pr-4 hover:text-gray-100 cursor-pointer rounded">Pricing</li></n-link>
+			<iframe style="transform: scale(.8);" src="https://ghbtns.com/github-btn.html?user=neutron-creative&repo=neutron&type=star&count=true&size=large" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
+		</ul>
+		-->
+        <ul class="ml-auto flex flex-row text-gray-300">
+          <!--<a href="https://client.neutron.so">
             <li class="p-2 pl-4 pr-4 hover:text-white cursor-pointer text-sm rounded">Login</li>
-          </a>
-          <a href="https://client.neutron.so/create-account">
-            <li class="flex ml-2 p-2 pl-4 pr-4 bg-indigo-600 hover:bg-indigo-500 cursor-pointer text-xs rounded text-white font-medium uppercase tracking-wide shadow justify-center items-center">Get started free</li>
+		  </a>-->
+          <a href="https://www.producthunt.com/upcoming/neutron">
+            <li class="flex ml-2 p-2 pl-4 pr-4 bg-indigo-600 hover:bg-indigo-500 cursor-pointer text-xs rounded text-white font-medium uppercase tracking-wide shadow justify-center items-center">Join the waitlist</li>
           </a>
         </ul>
       </div>
@@ -37,40 +39,58 @@
     <Nuxt />
     <section class="flex flex-col w-full nc-purple-bg items-center justify-content p-8 md:p-20 bg-black">
       <div class="flex flex-col md:flex-row w-full max-w-5xl justify-between">
-        <div class="flex flex-col">
-          <h2 class="text-white font-bold text-5xl mb-2">Ready to get started?</h2>
-          <p class="text-gray-300 text-xl max-w-2xl leading-relaxed">Join one of the fastest growing communities on the web in a mission to build better websites.</p>
+        <div class="flex flex-col items-start justify-center mr-auto text-left">
+          <h2 class="text-white font-bold text-5xl mb-2">Request access</h2>
+          <p class="text-gray-300 text-xl max-w-2xl leading-relaxed">Neutron is currently in private alpha, but we'd love to have you. Join the waitlist, and we'll send you an invite as soon as we're ready.</p>
         </div>
-        <div class="flex flex-col items-center justify-center mt-4 md:mt-0">
-          <button class="text-xs uppercase p-4 pl-6 pr-6 rounded bg-indigo-600 hover:bg-indigo-500 font-semibold text-white shadow tracking-wide w-full mb-4">Add your first website</button>
-          <button class="text-xs uppercase p-4 pl-6 pr-6 rounded bg-white hover:bg-gray-200 font-semibold text-black shadow tracking-wide w-full">Schedule a demo</button>
+        <div class="flex flex-col items-center text-center justify-center mt-4 md:mt-0">
+          <a href="https://www.producthunt.com/upcoming/neutron" class="text-xs uppercase p-4 pl-6 pr-6 rounded bg-indigo-600 hover:bg-indigo-500 font-semibold text-white shadow tracking-wide w-full mb-4">Join the waitlist</a>
+		  
+			<a href="https://neutroncreative.com/contact-us" class="text-xs uppercase p-4 pl-6 pr-6 rounded bg-white hover:bg-gray-200 font-semibold text-black shadow tracking-wide w-full">Get in touch</a>
         </div>
       </div>
     </section>
     <footer style="background:#111;" class="flex w-full justify-center text-center text-gray-600 text-sm mt-auto border border-black border-l-0 border-r-0 border-b-4 p-8">
       <div class="flex flex-col w-full max-w-5xl">
         <div class="flex flex-row w-full text-left">
-          <ul class="flex flex-col w-full lg:w-1/4 p-4">
-            <h4 class="text-gray-300 mb-4">Features</h4>
-            <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Neutron Web Suite</li>
-            <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Singlelink</li>
-            <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">WordPress Together</li>
-          </ul>
-          <ul class="flex flex-col w-full lg:w-1/4 p-4">
+          <ul class="flex flex-col w-full lg:w-1/3 p-4">
+            <h4 class="text-gray-300 mb-4">Products</h4>
+			<a href="#">
+				<li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Neutron</li>
+			</a>
+			<a href="https://singlelink.co">
+				<li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Singlelink</li>
+			</a>
+			<a href="https://www.producthunt.com/upcoming/wordpress-together">
+				<li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">WordPress Together</li>
+          	</a>
+			<a href="https://foss.neutron.so">
+				<li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Beautiful FOSS</li>
+			</a>
+		  </ul>
+          <ul class="flex flex-col w-full lg:w-1/3 p-4">
             <h4 class="text-gray-300 mb-4">Resources</h4>
-            <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Documentation</li>
-            <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Knowledgebase</li>
-            <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">API Reference</li>
-            <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">OSS (Open-Source Software)</li>
-          </ul>
-          <ul class="flex flex-col w-full lg:w-1/4 p-4">
+			<a href="https://github.com/Neutron-Creative">
+            	<li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Github</li>
+			</a>
+			<a href="https://discord.com/invite/3pBM4Px">
+            	<li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Discord</li>
+			</a>
+			<a href="https://neutroncreative.com/contact-us">
+				<li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Get Support</li>
+			</a>
+			<a href="https://neutroncreative.com/Neutron%20Creative%20Pitch%20Deck.pdf">
+            	<li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Fundraising</li>
+          	</a>
+		  </ul>
+          <!--<ul class="flex flex-col w-full lg:w-1/4 p-4">
             <h4 class="text-gray-300 mb-4">Application</h4>
             <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Login</li>
             <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Create an account</li>
             <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Reset your password</li>
             <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Get support</li>
-          </ul>
-          <ul class="flex flex-col w-full lg:w-1/4 p-4">
+		  </ul>-->
+          <ul class="flex flex-col w-full lg:w-1/3 p-4">
             <h4 class="text-gray-300 mb-4">Legal</h4>
             <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Terms of use</li>
             <li class="text-gray-500 hover:text-white cursor-pointer p-1 pl-0 pr-0 mb-2">Privacy policy</li>
@@ -98,6 +118,10 @@
   body {
     background:#111;
   }
+	html, body {
+		width: 100vw;
+		overflow-x: hidden;
+	}
 </style>
 
 <style>
--- www/pages/index.vue
+++ www/pages/index.vue
@@ -1,17 +1,53 @@
 <template>
   <section class="p-8 md:p-16 flex flex-col items-center justify-content bg-black">
     <div class="w-full max-w-4xl flex flex-col items-center justify-content text-center">
-      <h1 style="font-size: 6.25em;font-weight: 800;line-height: 1.1;letter-spacing: -4px;" class="max-w-4xl text-white mb-10">Put your websites on <span class="text-indigo-600" style="text-shadow: 0 0 65px rgba(88, 80, 236, .85);">autopilot</span></h1>
-      <div class="flex flex-col md:flex-row mb-10 w-full items-center justify-center">
-        <button class="flex flex-row items-center justify-center text-center rounded shadow text-white uppercase font-medium tracking-wide p-4 pl-6 pr-6 bg-indigo-600 hover:bg-indigo-500 mr-4">Add your first website free</button>
-        <button class="flex flex-row items-center justify-center text-center rounded shadow text-white uppercase font-medium tracking-wide p-4 pl-6 pr-6 bg-gray-900 hover:bg-gray-800 mr-4">View plans & pricing</button>
+      <h1 style="font-weight: 800;line-height: 1.1;letter-spacing: -4px;" class="text-6xl lg:text-8xl max-w-4xl text-white mb-8">Put your websites on <span class="text-indigo-600" style="text-shadow: 0 0 65px rgba(88, 80, 236, .85);">autopilot</span></h1>
+	  <!--
+		<div class="flex flex-col md:flex-row mb-10 w-full items-center justify-center">
+		  <button class="flex flex-row items-center justify-center text-center rounded shadow text-white uppercase font-medium tracking-wide p-4 pl-6 pr-6 bg-indigo-600 hover:bg-indigo-500 mr-4">Join the waitlist</button>
+			<button class="flex flex-row items-center justify-center text-center rounded shadow text-white uppercase font-medium tracking-wide p-4 pl-6 pr-6 bg-indigo-600 hover:bg-indigo-500 mr-4">Add your first website free</button>
+			<button class="flex flex-row items-center justify-center text-center rounded shadow text-white uppercase font-medium tracking-wide p-4 pl-6 pr-6 bg-gray-900 hover:bg-gray-800 mr-4">View plans &amp; pricing</button>
       </div>
-      <p class="text-gray-500 text-xl mb-32">Neutron combines enterprise-grade reliability and best-practices with an intuitive online workflow enabling teams to publish great content without worrying about their website.</p>
+		  -->
+      <p class="text-gray-500 text-xl mb-12">Neutron is the ultimate automated webmaster tool combined with an intuitive workflow, making your websites faster, more secure, and more affordable in one-click.</p>
+	  <button class="flex flex-row items-center justify-center text-center rounded shadow text-white uppercase font-medium tracking-wide p-4 pl-6 pr-6 bg-indigo-600 hover:bg-indigo-500 mb-32">Join the waitlist</button>
       <h3 style="letter-spacing: 2px;" class="uppercase text-white font-medium">Explore the Neutron way</h3>
       <div class="leadup"></div>
       <div class="circle mb-4">1</div>
-      <h3 class="text-indigo-600 text-3xl font-bold mb-2">Import</h3>
+      <h3 class="text-indigo-600 text-3xl font-bold mb-2">One-click Import</h3>
       <h2 class="text-white text-5xl font-bold">Start with your websites</h2>
+		<div class="flex flex-col lg:flex-row text-left text-gray-500 font-medium mb-16 text-xl justify-center items-center w-full max-w-4xl">
+			<div class="w-full lg:w-1/2 p-8">
+				<p>With one-click import, Neutron makes it easy to make your sites faster, more secure, and scalable in seconds with just one-click. Simply enter your domain name, and click!</p>
+			</div>
+			<img class="w-full lg:w-1/2 p-8 h-auto order-first lg:order-none" src="/add-site.png">
+		</div>
     </div>
+
+	<div class="leadup"></div>
+	<div class="circle mb-4">2</div>
+	<h3 class="text-indigo-600 text-3xl font-bold mb-2">Git-integrated site management</h3>
+	<h2 class="text-white text-5xl font-bold">Take control with versioned environments</h2>
+	<div class="flex flex-col lg:flex-row text-left text-gray-500 font-medium mb-16 text-xl justify-center items-center w-full max-w-4xl">
+		<img class="w-full lg:w-1/2 p-8 h-auto order-first lg:order-none" src="/git-websites.png" />
+		<div class="w-full lg:w-1/2 p-8">
+			<p>
+				Neutron brings the power of Git version control to every website. Audit website changes, manage staging &amp; production environments with branching, get real-time backups with one-click rollback, and deploy with confidence.
+			</p>
+		</div>
+	</div>
+	
+	<div class="leadup"></div>
+	<div class="circle mb-4">3</div>
+	<h3 class="text-indigo-600 text-3xl font-bold mb-2">Websites supercharged</h3>
+	<h2 class="text-white text-5xl font-bold">Faster, more secure, & infinity scalable</h2>
+	<div class="flex flex-col lg:flex-row text-left text-gray-500 font-medium mb-16 text-xl justify-center items-center w-full max-w-4xl">
+		<div class="w-full lg:w-1/2 p-8">
+			<p>
+				Websites on Neutron are better in every way by making proven enterprise solutions affordable &amp; one-click away. With servers up to 160x faster than the competition, deploy quicker with Neutron.
+			</p>
+		</div>
+		<img class="w-full lg:w-1/2 p-8 h-auto order-first lg:order-none" src="/git-websites.png" />
+	</div>
   </section>
 </template>
